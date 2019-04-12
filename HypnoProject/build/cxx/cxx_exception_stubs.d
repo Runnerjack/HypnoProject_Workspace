f9104647a8d90ffc6360a62ab076daedd7833f54
Jack <xgiax93@gmail.com>
2019-04-12 20:04:28 +0200

    removed folder

--- HypnoProject/build/cxx/cxx_exception_stubs.d
+++
@@ -1,6 +0,0 @@
-cxx_exception_stubs.o: \
- C:/msys32/home/slash/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
- C:/msys32/home/slash/esp/HypnoProject/build/include/sdkconfig.h
-
-C:/msys32/home/slash/esp/HypnoProject/build/include/sdkconfig.h:
-/home/slash/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
--- HypnoProject/main/includes/wifi_settings.h
+++
@@ -1,6 +0,0 @@
-// WiFi Connection
-#define SSID "hotspotPC"
-#define PASSPHARSE "qwerty123"
-#define MESSAGE "HelloTCPServer"
-#define TCPServerIP "192.168.137.1"
-#define TCPServerPort 3010
